cb1e2d13c59b832df6c75cab3a0a268a14e218b1
m <mm.marble02@gmail.com>
2022-09-26 15:20:39 -0600

    Added basic initial logic to determine winner, but does not reset or end game yet. Also added display for if it's X's or O's turn and who the winner is once that's determined.

--- gameboard.js
+++ gameboard.js
@@ -1,39 +1,117 @@
-
-
 (function() {
     let turn = "X";
+    let xMoves = [];
+    let oMoves = [];
+    let turnText = document.querySelector(".turn-text");
+    let winnerName = document.querySelector(".winner-name");
 
-    let gameboard = {
+    turnText.textContent = turn;
+
+    // make board state
+    let b = []
+    b.length = 9;
+    b.fill("");
+
+    const Player = (symbol) => {
+        const name = `Player ${symbol}`;
+        return {name, symbol}
+    }
+
+    const players = [Player("X"), Player("O")]
+
+    function toggleTurn() {
+        if (turn == "X") {
+            turn = "O";
+        } else {
+            turn = "X";
+        }
+        turnText.textContent = turn;
+    }
+
+    function updateBoard() {
+        const all_cells = document.querySelectorAll(".cell");
+        all_cells.forEach((c, i) => {
+                c.textContent = b[i];
+            })
+    }
+
+    function updatePlays(playerTurn, cellnum) {
+        if (playerTurn == "X") {
+            xMoves.push(cellnum);
+        } else if (playerTurn == "O") {
+            oMoves.push(cellnum);
+        }
+    }
+
+    function displayWinner(winner) {
+        winnerName.textContent = `Winner is ${winner}`;
+    }
+
+    function checkWin() {
+        const winStates = [
+            [0, 1, 2],
+            [3, 4, 5], 
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let w of winStates) {
+            xcount = 0;
+            ocount = 0;
+            for (let i of w) {
+                let string_i = String(i);
+                if (xMoves.includes(string_i)) {
+                    xcount += 1;
+                }
+                if (oMoves.includes(string_i)) {
+                    ocount += 1;
+                }
+                if (xcount == 3) {
+                    console.log("X wins");
+                    displayWinner("X");
+                    return "X"
+                }
+                if (ocount == 3) {
+                    console.log("O wins");
+                    displayWinner("O");
+                    return "O"
+                }
+            }
+        }
+    }
+
+    const gameboard = {
         cacheDOM: function() {
             this.board = document.querySelector(".board");
         },
         init: function() {
             this.cacheDOM();
+            for (let i = 0; i < 9; i++ ) {
+                let cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.dataset.index = i;
+                this.bindEvent(cell, this.b);
+                this.board.appendChild(cell);
+            }
         },
         bindEvent: function(element) {
             element.addEventListener("click", this.handleClick);
         },
-        render: function() {
-            for (let i = 0; i < 9; i++ ) {
-                let cell = document.createElement("div");
-                cell.classList.add("cell");
-                this.bindEvent(cell);
-                this.board.appendChild(cell);
-            }
-        },
         handleClick: function() {
-            if (!this.textContent) {
-                this.textContent = turn;
-                if (turn == "X") {
-                    turn = "O";
-                } else {
-                    turn = "X";
-                }
+            console.log(b[this.dataset.index], b[this.dataset.index] == null)
+            if (b[this.dataset.index] == "") {
+                b[this.dataset.index] = turn;
+                updatePlays(turn, this.dataset.index);
+                toggleTurn();
             }
+            updateBoard();
+            checkWin();
         },
     }
 
     gameboard.init();
-    gameboard.render();
 
 })(); 
